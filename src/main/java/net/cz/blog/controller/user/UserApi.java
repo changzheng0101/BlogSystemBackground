76ee57bffc4d086bc01fd28e1afb8afea26de64d
changzheng0101 <59946486+changzheng0101@users.noreply.github.com>
2021-10-25 15:55:59 +0800

    create user api

---
+++ src/main/java/net/cz/blog/controller/user/UserApi.java
@@ -0,0 +1,64 @@
+package net.cz.blog.controller.user;
+
+
+import lombok.extern.slf4j.Slf4j;
+import net.cz.blog.Response.ResponseResult;
+import net.cz.blog.pojo.BlogUser;
+import org.springframework.web.bind.annotation.*;
+
+@Slf4j
+@RestController
+@RequestMapping("/user")
+public class UserApi {
+    //   初始化管理员账号
+    @PostMapping("/admin_account")
+    public ResponseResult initManagerAccount(@RequestBody BlogUser user) {
+        log.info("user name==>" + user.getUser_name());
+        log.info("password==>" + user.getPassword());
+        log.info("email ==> " + user.getEmail());
+
+        return ResponseResult.SUCCESS();
+    }
+
+    //注册
+    @PostMapping
+    public ResponseResult register(@RequestBody BlogUser user) {
+        return null;
+    }
+
+    //登录   加验证码  测试是否是人类
+    @PostMapping("/{captcha}")
+    public ResponseResult login(@PathVariable("captcha") String captcha, @RequestBody BlogUser user) {
+        return null;
+    }
+
+    //获取图灵验证码
+    @GetMapping("/captcha")
+    public ResponseResult getCaptcha() {
+        return null;
+    }
+
+    //发送邮件
+    @GetMapping("/verify_code")
+    public ResponseResult sendVerifyCode(@RequestParam("email") String email) {
+        return null;
+    }
+
+    //修改密码 put请求用于更新
+    @PutMapping("/password")
+    public ResponseResult updatePassword(@RequestBody BlogUser user) {
+        return null;
+    }
+
+    //获取用户信息
+    @GetMapping("/{userId}")
+    public ResponseResult getUserInfo(@PathVariable("userId") String userId) {
+        return null;
+    }
+
+    //更新用户
+    @PutMapping
+    public ResponseResult updateUserInfo(@RequestBody BlogUser user) {
+        return null;
+    }
+}
