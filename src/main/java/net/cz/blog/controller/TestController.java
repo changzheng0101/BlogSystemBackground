f1f39eaf75594d5b19fff9c566397c95d09a41f0
changzheng0101 <59946486+changzheng0101@users.noreply.github.com>
2021-10-25 15:15:32 +0800

    config log

--- src/main/java/net/cz/blog/controller/TestController.java
+++ src/main/java/net/cz/blog/controller/TestController.java
@@ -1,24 +1,29 @@
 package net.cz.blog.controller;
 
 
+import lombok.extern.slf4j.Slf4j;
 import net.cz.blog.Response.ResponseResult;
-import net.cz.blog.Response.ResponseState;
 import net.cz.blog.pojo.House;
 import net.cz.blog.pojo.User;
+import org.slf4j.LoggerFactory;
 import org.springframework.web.bind.annotation.*;
 
+import java.util.logging.Logger;
+
+@Slf4j
 @RestController  //不用声明 @ResponseBody -->这个代表返回的是json对象 不是一个页面
 @RequestMapping("/test")  //代表所有文件都在test目录之下
 public class TestController {
+
     @RequestMapping(value = "/hello-world", method = RequestMethod.GET)
     public ResponseResult helloWorld() {
-        System.out.println("this is helloWorld from Spring boot");
+        log.info("this is helloWorld from Spring boot");
         return ResponseResult.SUCCESS().setData("hello world");
     }
 
     @GetMapping("/test-json")
     public ResponseResult getJson() {
-        System.out.println("this is json data test");
+        log.info("this is json data test");
         User user = new User("json", 18);
         House house = new House("baiGong", "china");
         user.setHouse(house);
