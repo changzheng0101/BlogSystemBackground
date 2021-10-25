ff61d215c01bd477c580b149697699e4260846bc
changzheng0101 <59946486+changzheng0101@users.noreply.github.com>
2021-10-25 16:08:30 +0800

    create swagger ui

--- src/main/java/net/cz/blog/BlogApplication.java
+++ src/main/java/net/cz/blog/BlogApplication.java
@@ -3,7 +3,9 @@ package net.cz.blog;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import springfox.documentation.swagger2.annotations.EnableSwagger2;
 
+@EnableSwagger2
 @SpringBootApplication()
 public class BlogApplication {
     public static void main(String[] args) {
