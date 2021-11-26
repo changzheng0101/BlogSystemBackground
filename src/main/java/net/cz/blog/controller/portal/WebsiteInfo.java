6fd35f9391ee6af8de455c28600997509ecfac53
changzheng0101 <7589854+changzheng0101@user.noreply.gitee.com>
2021-11-26 10:40:55 +0800

    count

--- src/main/java/net/cz/blog/controller/portal/WebsiteInfo.java
+++ src/main/java/net/cz/blog/controller/portal/WebsiteInfo.java
@@ -8,6 +8,7 @@ import net.cz.blog.services.ILooperService;
 import net.cz.blog.services.IWebsiteInfoService;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PutMapping;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
 
@@ -52,5 +53,22 @@ public class WebsiteInfo {
     @GetMapping("/friend_link")
     public ResponseResult getFriendLink() {
         return friendLinkService.getFriendLinkList();
+    }
+
+    /**
+     * 统计访问量
+     * 1.每个页面都会增加访问量
+     * 2.应该按照ip过滤
+     * 3.可以集成第三方工具进行统计
+     * <p>
+     * 统计信息保存在redis中，同时mysql中也有保存
+     * 当用户获取统计信息的时候，才从mysql中进行获取
+     * <p>
+     * redis：每个页面访问时，没有就去mysql中拿，有就自增
+     * mysql：用户访问总访问量的时候
+     */
+    @PutMapping("/view_count")
+    public void updateViewCount() {
+        websiteInfoService.updateViewCount();
     }
 }
--- src/main/java/net/cz/blog/services/IWebsiteInfoService.java
+++ src/main/java/net/cz/blog/services/IWebsiteInfoService.java
@@ -14,4 +14,5 @@ public interface IWebsiteInfoService {
 
     ResponseResult getWebsiteTitle();
 
+    void updateViewCount();
 }
